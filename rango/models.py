ee947d1d5f0d17f94fe4d3a7a7f795753ba77d27
Abraham Escalante <aeklant@gmail.com>
2015-01-01 14:11:34 -0600

    Added a user profile model

--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 
 class Category( models.Model ):
@@ -7,13 +8,13 @@ class Category( models.Model ):
 	likes = models.IntegerField(default=0)
 	slug = models.SlugField(unique=True)
 	
-	def save(self, *args, **kwargs): 
+	def save( self, *args, **kwargs ): 
 		self.slug = slugify(self.name)
 
 		# Calling super has implications that make it not very safe, it's not like java.		# We will call the parent constructor directly.
 		models.Model.save(self, *args, **kwargs)
 		
-	def __unicode__(self):
+	def __unicode__( self ):
 		return self.name
 
 class Page( models.Model ):
@@ -22,5 +23,16 @@ class Page( models.Model ):
 	url = models.URLField()
 	views = models.IntegerField(default=0)
 
-	def __unicode__(self):
+	def __unicode__( self ):
 		return self.title
+
+class UserProfile( models.Model ):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField( User )
+
+    # The additional attributes we wish to include.
+    website = models.URLField( blank=True )
+    picture = models.ImageField( upload_to='profile_images', blank=True )
+
+    def __unicode__( self ):
+        return self.user.username
