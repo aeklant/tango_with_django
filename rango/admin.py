00729bd762a40c5b67a4542731ed7875bc83c81a
Abraham Escalante <aeklant@gmail.com>
2015-01-01 14:17:41 -0600

    Added 'UserProfile' to rango admin

--- rango/admin.py
+++ rango/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 # Add in this class to customize the Admin Interface
-class CategoryAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug':('name',)}
+class CategoryAdmin( admin.ModelAdmin ):
+	prepopulated_fields = { 'slug':( 'name', ) }
 
 # Register models so they show up in the admin tool
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Page)
+admin.site.register( Category, CategoryAdmin )
+admin.site.register( Page )
+admin.site.register( UserProfile )
