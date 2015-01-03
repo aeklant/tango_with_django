53af3400f75821a5cef22093092c71f4424d2cf3
Abraham Escalante <aeklant@gmail.com>
2015-01-03 11:15:31 -0600

    Added register and login capabilities.

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from rango.models import Category, Page
+from django.contrib.auth.models import User
+from rango.models import Category, Page, UserProfile
 
 class CategoryForm( forms.ModelForm ):
     name = forms.CharField( max_length=128, help_text='Please enter the category name' )
@@ -11,9 +12,9 @@ class CategoryForm( forms.ModelForm ):
     class Meta:
         # Provide an association between the ModelForm and model.
         model = Category
-        fields = ('name',)
+        fields = ( 'name', )
 
-class PageForm(forms.ModelForm):
+class PageForm( forms.ModelForm ):
     title = forms.CharField( max_length=128, help_text='Please enter the title of the page' )
     url = forms.URLField( max_length=200, help_text='Please enter the URL of the page' )
     views = forms.IntegerField( widget=forms.HiddenInput(), initial=0 )
@@ -27,8 +28,19 @@ class PageForm(forms.ModelForm):
         # Some fields may allow NULL values, so we may want to include them...
         # Here we are hiding the foreign key.
         # We can either exclude the category field from the form, 
-        exclude = ('category',)
+        exclude = ( 'category', )
         
-        # or specify the fields to include (i.e. not include the category field)
-        # fields = ('title', 'url', 'views')
+        # or specify the fields to include ( i.e. not include the category field )
+        # fields = ( 'title', 'url', 'views' )
         
+class UserForm( forms.ModelForm ):
+    password = forms.CharField( widget=forms.PasswordInput() )
+
+    class Meta:
+        model = User
+        fields = ( 'username', 'email', 'password' )
+
+class UserProfileForm( forms.ModelForm ):
+    class Meta:
+        model = UserProfile
+        fields = ( 'website', 'picture' )
